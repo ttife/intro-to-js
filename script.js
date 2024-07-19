d26f7fe63a6aad038ce79e2ecf8278454de9aceb
ttife <tifebolu220@gmail.com>
2024-07-19 13:00:41 +0100

    changes

---
+++ script.js
@@ -0,0 +1,53 @@
+// alert("Welcome to todays class")
+
+let firstName = 'My name is Tife'
+
+firstName = 'Boluwatife'
+
+const surname = 'Mufutau'
+
+var username = 'Tife'
+console.log(username)
+
+alert('My name is ' + firstName + ' ' + surname)
+
+
+let tribe = 'Calabar'
+tribe = 'oyo'
+const Nationality = 'Nigerian'
+console.log(tribe)
+
+let Tife = {
+    firstName: "Tife",
+    surname: "Mufutau",
+    age: 30,
+    Nationality: 'spanish',
+    city: 'Barcelona',
+    drive: true,
+}
+console.log(Tife)
+
+let age = 30;
+let school = 'Bells University';
+let full = 'true';
+let country;
+let state = null;
+let PI = 3.1422;
+let BigInt = 3000000000000;
+
+const addition = 2024 + 2004;
+const subtraction = 2024 - 2020;
+const multiplication = 20 * 4;
+const division = 20 / 2;
+console.log(addition, subtraction, multiplication, division)
+
+// TEMPLATE LITERAL
+
+console.log('This is addition', addition);
+console.log('This is addition ${addition} on line 47');
+console.log('This is subtraction', subtraction);
+console.log('This is multiplication', multiplication);
+console.log('This is division', division);
+
+
+
